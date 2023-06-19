1ca5f4f9cb86253715710201529987d4a9a16bc0
Lily Tian <lilytian@Lily-Tians-Datatonic-MacBook-Pro.local>
2023-06-19 14:14:19 +0100

    add better solutions

--- pyspark-task/main_pandas.py
+++ pyspark-task/main_pandas.py
@@ -11,6 +11,8 @@ df2 = pd.read_csv('dataset2.csv')
 df3_first_merge = pd.merge(df1, df2, on='counter_party', how='inner')
 
 
+# -----------------------------------------------------------------------------------------------------------------------
+# SOLUTION 1
 # Generate desired output
 # legal_entity, counterparty, tier, max(rating by counterparty), sum(value where status=ARAP),
 # sum(value where status=ACCR)
@@ -32,7 +34,7 @@ df4_join = pd.concat([df4_arap, df4_accr], axis=0, ignore_index=False)
 df4_join['total_all_status'] = df4_join['sum_value'] \
     .groupby(['legal_entity', 'counter_party', 'tier']) \
     .transform(lambda x: x.sum())
-print(df4_join)
+# print(df4_join)
 #                                  max_rating  sum_value status  total_all_status
 # legal_entity counter_party tier
 # L1           C1            1            3.0       40.0   ARAP              40.0
@@ -51,3 +53,27 @@ print(df4_join)
 #              C5            5            4.0      115.0   ACCR            1115.0
 # L3           C3            3            4.0      145.0   ACCR             145.0
 #              C6            6            6.0       60.0   ACCR             205.0
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# BETTER SOLUTIONS
+# Using pivot_table
+df4_sum = df3_first_merge.pivot_table(index=['legal_entity', 'counter_party', 'tier'], columns='status', values='value',
+                                      aggfunc='sum').rename(columns={'ARAP': 'sum_ARAP', 'ACCR': 'sum_ACCR'})
+df4_max = pd.DataFrame(df3_first_merge.groupby(['legal_entity', 'counter_party', 'tier']).agg({'rating': 'max'}))
+df4_max.rename(columns={'rating': 'max_rating'}, inplace=True)
+
+df5 = pd.merge(df4_max, df4_sum, on=['legal_entity', 'counter_party', 'tier'], how='inner').fillna(0)
+df5['total_all_status'] = df5['sum_ARAP'] + df5['sum_ACCR']
+
+print(df5)
+#                                  max_rating  sum_ACCR  sum_ARAP  total_all_status
+# legal_entity counter_party tier
+# L1           C1            1              3       0.0      40.0              40.0
+#              C3            3              6       0.0       5.0               5.0
+#              C4            4              6     100.0      40.0             140.0
+# L2           C2            2              3      40.0      20.0              60.0
+#              C3            3              2      52.0       0.0              52.0
+#              C5            5              6     115.0    1000.0            1115.0
+# L3           C3            3              4     145.0       0.0             145.0
+#              C6            6              6      60.0     145.0             205.0
--- pyspark-task/main_pyspark.py
+++ pyspark-task/main_pyspark.py
@@ -1,7 +1,7 @@
 # Framework 3: pyspark
 
 from pyspark.sql import SparkSession, Window
-from pyspark.sql.functions import sum as spark_sum
+from pyspark.sql.functions import sum as spark_sum, col
 
 spark = SparkSession.builder.appName('datasetExercise').getOrCreate()
 
@@ -12,10 +12,11 @@ df2 = spark.read.csv('dataset2.csv', header=True, inferSchema=True)
 # Merge the two dataframes
 df3_first_merge = df1.join(df2, on='counter_party', how='inner')
 
+# -----------------------------------------------------------------------------------------------------------------------
+# SOLUTION 1
 # Generate desired output
 # legal_entity, counterparty, tier, max(rating by counterparty), sum(value where status=ARAP),
 # sum(value where status=ACCR)
-# create new record to add total for each of legal entity, counterparty & tier.
 df3_groupby = df3_first_merge \
     .groupby(['legal_entity', 'counter_party', 'tier', 'status']) \
     .agg({'rating': 'max', 'value': 'sum'}) \
@@ -27,13 +28,14 @@ df3_sum_arap = df3_first_merge \
     .groupby(['legal_entity', 'counter_party', 'tier']) \
     .sum('value') \
     .withColumnRenamed('sum(value)', 'sum_value_arap')
-print(df3_sum_arap.show())
+# print(df3_sum_arap.show())
 
+# create new record to add total for each of legal entity, counterparty & tier.
 df4_add_total = df3_groupby.withColumn('total_all_status',
                                        spark_sum('sum_value').over(
                                            Window.partitionBy('legal_entity', 'counter_party', 'tier')))
 
-print(df4_add_total.show())
+# print(df4_add_total.show())
 # +------------+-------------+----+------+----------+---------+----------------+
 # |legal_entity|counter_party|tier|status|max_rating|sum_value|total_all_status|
 # +------------+-------------+----+------+----------+---------+----------------+
@@ -50,3 +52,31 @@ print(df4_add_total.show())
 # |          L3|           C6|   6|  ACCR|         6|       60|             205|
 # |          L3|           C6|   6|  ARAP|         5|      145|             205|
 # +------------+-------------+----+------+----------+---------+----------------+
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# BETTER SOLUTIONS
+# Try out the pivot and unpivot functions in pyspark
+df3_max = df3_first_merge.groupby(['legal_entity', 'counter_party', 'tier']).max(
+    "rating").withColumnRenamed(
+    'max(rating)', 'max_rating')
+df3_sum = df3_first_merge.groupby(['legal_entity', 'counter_party', 'tier']).pivot('status').sum(
+    'value').withColumnRenamed('ACCR', 'sum_value_accr').withColumnRenamed('ARAP', 'sum_value_arap')
+
+df4 = df3_max.join(df3_sum, on=['legal_entity', 'counter_party', 'tier'])
+
+# create new record to add total for each of legal entity, counterparty & tier.
+df5 = df4.na.fill(0).withColumn('total_all_status', col('sum_value_accr') + col('sum_value_arap'))
+df5.show()
+# +------------+-------------+----+----------+--------------+--------------+----------------+
+# |legal_entity|counter_party|tier|max_rating|sum_value_accr|sum_value_arap|total_all_status|
+# +------------+-------------+----+----------+--------------+--------------+----------------+
+# |          L2|           C2|   2|         3|            40|            20|              60|
+# |          L1|           C1|   1|         3|             0|            40|              40|
+# |          L1|           C3|   3|         6|             0|             5|               5|
+# |          L2|           C3|   3|         2|            52|             0|              52|
+# |          L3|           C3|   3|         4|           145|             0|             145|
+# |          L2|           C5|   5|         6|           115|          1000|            1115|
+# |          L1|           C4|   4|         6|           100|            40|             140|
+# |          L3|           C6|   6|         6|            60|           145|             205|
+# +------------+-------------+----+----------+--------------+--------------+----------------+
