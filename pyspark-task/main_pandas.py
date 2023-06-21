a542b2030f76f02c4195c8949acd0badeab6a369
lilsgit <lilytianyi7@gmail.com>
2023-06-21 16:35:42 +0100

    refactor: add functions

--- pyspark-task/main_pandas.py
+++ pyspark-task/main_pandas.py
@@ -10,20 +10,42 @@ df2 = pd.read_csv('dataset2.csv')
 df3_first_merge = pd.merge(df1, df2, on='counter_party', how='inner')
 
 
+# function to calculate the metrics
+def calculate_metrics(dataframe, max_col, sum_col, filter_col, filter_condition1, filter_condition2):
+    max_v = (max_col, 'max')
+    sum_value_cond1 = (sum_col, lambda x: x[dataframe[filter_col] == filter_condition1].sum())
+    sum_value_cond2 = (sum_col, lambda x: x[dataframe[filter_col] == filter_condition2].sum())
+
+    return {
+        f"max_{max_col}": max_v,
+        f"sum_{sum_col}_{filter_condition1}": sum_value_cond1,
+        f"sum_{sum_col}_{filter_condition2}": sum_value_cond2
+    }
+
+
+# all the desired columns
+desired_columns = ['legal_entity', 'counter_party', 'tier', 'max_rating', 'sum_value_ARAP', 'sum_value_ACCR']
+
+
 # function to aggregate the data
 def agg_group_by_cols(group_by_cols: [str], the_other_cols: [str]):
     agg_dict = {
         f"{col}": (col, 'count') for col in the_other_cols
     }
-    agg_dict.update({
-        "max_rating": ('rating', 'max'),
-        "sum_of_ARAP": ('value', lambda x: x[df3_first_merge['status'] == 'ARAP'].sum()),
-        "sum_of_ACCR": ('value', lambda x: x[df3_first_merge['status'] == 'ACCR'].sum())
-    })
-    return df3_first_merge.groupby(group_by_cols).agg(**agg_dict).reset_index()[
-        ['legal_entity', 'counter_party', 'tier', 'max_rating', 'sum_of_ARAP', 'sum_of_ACCR']]
+    agg_dict.update(calculate_metrics(df3_first_merge, 'rating', 'value', 'status', 'ARAP', 'ACCR'))
+
+    return df3_first_merge.groupby(group_by_cols).agg(**agg_dict).reset_index()[desired_columns]
 
 
+def union_all_dataframes(all_mappings_list: [([str], [str])]):
+    all_dataframes = []
+    for group_by_cols, the_other_cols in all_mappings_list:
+        df = agg_group_by_cols(group_by_cols, the_other_cols)
+        all_dataframes.append(df)
+    return pd.concat(all_dataframes, axis=0, ignore_index=False)
+
+
+# all the group by pairs needed
 all_mappings = [
     (['legal_entity'], ['counter_party', 'tier']),
     (['counter_party'], ['legal_entity', 'tier']),
@@ -33,11 +55,7 @@ all_mappings = [
     (['counter_party', 'tier'], ['legal_entity'])
 ]
 
-all_dataframes = []
-for group_by_cols, the_other_cols in all_mappings:
-    df = agg_group_by_cols(group_by_cols, the_other_cols)
-    all_dataframes.append(df)
-df5 = pd.concat(all_dataframes, axis=0, ignore_index=False)
+df5 = union_all_dataframes(all_mappings)
 print(df5)
 #   legal_entity counter_party  tier  max_rating  sum_of_ARAP  sum_of_ACCR
 # 0           L1             6     6           6           85          100
